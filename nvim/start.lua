d5469f4229a7d19b8c901d87c3c23791ee0f69ce
stefan.pinter <stefan.pinter@bearingpoint.com>
2025-07-01 08:44:24 +0200

    add empty nvim config start.lua

--- nvim/start.lua
+++ nvim/start.lua
@@ -1,11 +1,3 @@
-require "dailynotes".setup({
-    path = '/Users/stefan.pinter/OneDrive - BearingPoint GmbH/Obsidian Vault/daily/'
-})
-
-vim.keymap.set("n", "<C-p>", ":DailyPrev<CR>")
-vim.keymap.set("n", "<C-n>", ":DailyNext<CR>")
-
-vim.keymap.set("n", "<C-CR>", ":lua require('toggle-checkbox').toggle()<CR>")
 
 
 
